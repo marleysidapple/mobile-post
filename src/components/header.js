dbc9fedf4e7712a3c7c56e71445e56dc59a95639
marleysidapple <bsiddhartha25@gmail.com>
2017-05-24 08:38:56 +0800

    fix

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,9 @@ class Header extends Component {
 		//get the props by doing {this.props.propname}
 		return (
 				<View style={styles.headerContainer}>
-				<Text style={styles.welcome}>{this.props.title}</Text>
+					<Text style={styles.links}>More</Text>
+					<Text style={styles.welcome}>{this.props.title}</Text>
+					<Text style={styles.links}>Back</Text>
 				</View>
 			);
 	}
@@ -27,8 +29,8 @@ const styles = {
     headerContainer: {
     	flexDirection: 'row',
 	  	backgroundColor: '#f8f8f8',
-	  	justifyContent: 'center',
-	  	alignItems: 'center',
+	  	//justifyContent: 'center',
+	  	//alignItems: 'center',
 	  	height: 60,
 	  	paddingTop: 20,
 	  	shadowColor: '#000',
@@ -38,9 +40,17 @@ const styles = {
 	  	position: 'relative'
 	  },
 	welcome: {
+		flex: 1,
 	  	fontSize: 16,
 	  	color: '#43536F',
-	  }
+	  	textAlign: 'center'
+	  },
+
+	links: {
+		textAlign:'center',
+    	width: 64,
+    	color: '#43536F',
+	}
 };
 
 export default Header;
